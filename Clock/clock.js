0edb8c5f1f092466e8ea50804760c2b237b0d6b0
Shoukath Khan <sfazlullakhan@expedia.com>
2017-11-09 11:00:36 -0600

    Clock Added

--- Clock/clock.js
+++ Clock/clock.js
@@ -9,3 +9,22 @@ document.querySelector('#clock .content').innerHTML = clock.getTime();
 setInterval(function () {
 	document.querySelector('#clock .content').innerHTML = clock.getTime();
 }, 1000 * 15)
+
+
+
+
+document.fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.documentElement.webkitRequestFullScreen;
+
+function requestFullscreen(element) {
+	if (element.requestFullscreen) {
+		element.requestFullscreen();
+	} else if (element.mozRequestFullScreen) {
+		element.mozRequestFullScreen();
+	} else if (element.webkitRequestFullScreen) {
+		element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+	}
+}
+
+if (document.fullscreenEnabled) {
+	requestFullscreen(document.documentElement);
+}
