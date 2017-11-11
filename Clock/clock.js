ffb205598fb306d29026de754947a859168ddeff
Shoukath Khan <sfazlullakhan@expedia.com>
2017-11-11 14:40:32 -0600

    Updating Clock

--- Clock/clock.js
+++ Clock/clock.js
@@ -35,13 +35,14 @@ function clickHandler() {
 	}
 }
 
-document.querySelector('#clock .content').addEventListener('click', clickHandler, false);
+document.querySelector('#upper-container').addEventListener('click', clickHandler, false);
 
 var getWeatherInfo = function () {
-	$.ajax('http://api.wunderground.com/api/fecf8ea800958a0e/conditions/q/CA/60089.json')
-		.then(function(response) {
-			$('#weather').html(Math.round(response.current_observation.temp_f) + '&deg;');
-		});
+	// $.ajax('http://api.wunderground.com/api/fecf8ea800958a0e/conditions/q/CA/60089.json')
+	// 	.then(function(response) {
+	// 		$('#weather').html(Math.round(response.current_observation.temp_f) + '&deg;');
+	// 	});
+	$('#weather').html(35 + '&deg;');
 };
 
 setInterval(getWeatherInfo, 1000 * 60 * 15);
