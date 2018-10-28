9f29fb919136b655a95253f56e49e685c3adfc47
Shoukath Khan <sfazlullakhan@CHIC02W80P4HTD7.sea.corp.expecn.com>
2018-10-28 15:54:01 -0500

    hk: making the logo fill to black sooner

--- hk-designs/scripts/scroll.js
+++ hk-designs/scripts/scroll.js
@@ -1,15 +1,16 @@
-$(document).scroll(function () {
+const applyLogoFill = function () {
     const heroHeight = $('.hero').height();
     const documentPosition = $(document).scrollTop();
     const $logo = $('#logo-svg');
-    if (documentPosition > heroHeight) {
+    if (documentPosition >= heroHeight) {
         $logo.css({fill: 'black'});
     } else {
         $logo.css({fill: ''});
     }
-})
+}
+$(document).scroll(applyLogoFill);
 
-$('.hero .down-arrow').click(function() {
+$('.hero .down-arrow').click(() => {
     const heroHeight = $('.hero').height();
     $('html, body').animate({scrollTop: heroHeight}, 500);
 })
