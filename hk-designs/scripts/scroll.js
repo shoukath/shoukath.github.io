596b53d2dd78eb3cee4e9ad1138e1a64c58a88da
Shoukath Khan <sfazlullakhan@CHIC02W80P4HTD7.sea.corp.expecn.com>
2018-10-31 09:51:10 -0500

    hk: Adding the scroll to top control

--- hk-designs/scripts/scroll.js
+++ hk-designs/scripts/scroll.js
@@ -4,8 +4,10 @@ const applyLogoFill = function () {
     const $logo = $('#logo-svg');
     if (documentPosition >= heroHeight) {
         $logo.css({fill: 'black'});
+        $('.up-arrow').css({visibility: 'visible'});
     } else {
         $logo.css({fill: ''});
+        $('.up-arrow').css({visibility: 'hidden'});
     }
 }
 $(document).scroll(applyLogoFill);
@@ -13,4 +15,8 @@ $(document).scroll(applyLogoFill);
 $('.hero .down-arrow').click(() => {
     const heroHeight = $('.hero').height();
     $('html, body').animate({scrollTop: heroHeight}, 500);
-})
+});
+
+$('.up-arrow').click(() => {
+    $('html, body').animate({scrollTop: 0}, 500);
+});
